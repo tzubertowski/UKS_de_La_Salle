f2832b9e89447fa1db88ee046f48e7fb46f54a11
Piotr Kulas <pkulas@sigma.ug.edu.pl>
2013-11-25 15:59:50 +0100

    Dodałem więcej tabel SQL, potrzebnych na obecną chwilę, model bazy danych

---
+++ dokumentacja/all.sql
@@ -0,0 +1,179 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
+
+
+-- -----------------------------------------------------
+-- Table `user`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `user` ;
+
+CREATE  TABLE IF NOT EXISTS `user` (
+  `id` INT NOT NULL ,
+  `first` VARCHAR(45) NULL ,
+  `last` VARCHAR(45) NULL ,
+  `password` VARCHAR(32) NULL ,
+  PRIMARY KEY (`id`) )
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `rank`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `rank` ;
+
+CREATE  TABLE IF NOT EXISTS `rank` (
+  `id` VARCHAR(50) NOT NULL ,
+  `rank` INT NULL ,
+  PRIMARY KEY (`id`) )
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `user_rank`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `user_rank` ;
+
+CREATE  TABLE IF NOT EXISTS `user_rank` (
+  `id_user` INT NOT NULL ,
+  `id_rank` VARCHAR(50) NOT NULL ,
+  UNIQUE INDEX `id_user_UNIQUE` (`id_user` ASC) ,
+  INDEX `fk_user_rank_1` (`id_user` ASC) ,
+  INDEX `fk_user_rank_2` (`id_rank` ASC) ,
+  CONSTRAINT `fk_user_rank_1`
+    FOREIGN KEY (`id_user` )
+    REFERENCES `user` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_rank_2`
+    FOREIGN KEY (`id_rank` )
+    REFERENCES `rank` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `category`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `category` ;
+
+CREATE  TABLE IF NOT EXISTS `category` (
+  `id` INT NOT NULL ,
+  `title` VARCHAR(50) NULL ,
+  `description` VARCHAR(255) NULL ,
+  `weight` INT NULL ,
+  PRIMARY KEY (`id`) )
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `subcategory_type`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `subcategory_type` ;
+
+CREATE  TABLE IF NOT EXISTS `subcategory_type` (
+  `id` INT NOT NULL ,
+  `title` VARCHAR(255) NULL ,
+  `description` VARCHAR(255) NULL ,
+  PRIMARY KEY (`id`) )
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `subcategory`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `subcategory` ;
+
+CREATE  TABLE IF NOT EXISTS `subcategory` (
+  `id` INT NOT NULL ,
+  `title` VARCHAR(50) NULL ,
+  `description` VARCHAR(255) NULL ,
+  `weight` INT NULL ,
+  `id_category` INT NULL ,
+  `id_subcategory_type` INT NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `fk_subcategory_1` (`id_category` ASC) ,
+  INDEX `fk_subcategory_2` (`id_subcategory_type` ASC) ,
+  CONSTRAINT `fk_subcategory_1`
+    FOREIGN KEY (`id_category` )
+    REFERENCES `category` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_subcategory_2`
+    FOREIGN KEY (`id_subcategory_type` )
+    REFERENCES `subcategory_type` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `news`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `news` ;
+
+CREATE  TABLE IF NOT EXISTS `news` (
+  `id` INT NOT NULL ,
+  `id_user` INT NULL ,
+  `id_category` INT NULL ,
+  `title` VARCHAR(50) NULL ,
+  `content` TEXT NULL ,
+  `date_added` DATETIME NULL ,
+  `date_modified` DATETIME NULL ,
+  `user_modified` INT NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `fk_news_1` (`id_user` ASC) ,
+  INDEX `fk_news_2` (`id_category` ASC) ,
+  INDEX `fk_news_3` (`user_modified` ASC) ,
+  CONSTRAINT `fk_news_1`
+    FOREIGN KEY (`id_user` )
+    REFERENCES `user` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_news_2`
+    FOREIGN KEY (`id_category` )
+    REFERENCES `category` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_news_3`
+    FOREIGN KEY (`user_modified` )
+    REFERENCES `user` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `calendar`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `calendar` ;
+
+CREATE  TABLE IF NOT EXISTS `calendar` (
+  `id` INT NOT NULL ,
+  `id_user` INT NULL ,
+  `id_category` INT NULL ,
+  `title` VARCHAR(50) NULL ,
+  `description` VARCHAR(255) NULL ,
+  `date_start` DATETIME NULL ,
+  `date_end` DATETIME NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `fk_calendar_1` (`id_user` ASC) ,
+  INDEX `fk_calendar_2` (`id_category` ASC) ,
+  CONSTRAINT `fk_calendar_1`
+    FOREIGN KEY (`id_user` )
+    REFERENCES `user` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_calendar_2`
+    FOREIGN KEY (`id_category` )
+    REFERENCES `category` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
